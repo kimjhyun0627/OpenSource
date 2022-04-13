a033abf41ce86f12b898633a201548f0d14b82ad
kimjhyun <kimjhyun0627@gmail.com>
2022-04-13 19:09:41 +0900

    commit_5

--- HW05/word_freq.py
+++ HW05/word_freq.py
@@ -3,19 +3,22 @@ import re
 import operator
 
 filepath = sys.argv[1]
-num = sys.argv[2]
+num = int(sys.argv[2])
 dic = {}
 
 with open(filepath, "r") as file:
     lines = file.readlines()
 
 for ii in lines:
-    ii=re.sub("[-+=!@#$%^&*\"~`,.?/]"," ",ii)
+    ii = re.sub("[-+=!@#$:;%^&*\"~`,.?/]", " ", ii)
     words = ii.split()
     for cnt in words:
-        dic[cnt] = dic.get(cnt,0)+1
+        dic[cnt] = dic.get(cnt, 0) + 1
 
 dic = sorted(dic.items(), key=operator.itemgetter(1))
 
-for i in range(dic.count(), num):
-    print("%10s %-5s"%(dic[i][0],dic[i][1]))
+print(dic)
+for ii in range(len(dic) - 1, -1, -1):
+    if (dic[ii][1] <= num):
+        break
+    print("%-10s %5s" % (dic[ii][0], dic[ii][1]))
