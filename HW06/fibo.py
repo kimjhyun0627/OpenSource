bb5e37617f118da2f045bf393aa020735facd760
kimjhyun <kimjhyun0627@gmail.com>
2022-04-24 20:44:14 +0900

    change fibo

--- HW06/fibo.py
+++ HW06/fibo.py
@@ -1,20 +1,19 @@
-print("fibonacci number: ")
-num = int(input())
+num = int(input("fibonacci number: "))
 
 fiboList = [0 for i in range(num + 1)]
 fiboList[0] = 0
 fiboList[1] = 1
 for i in range(2, num + 1):
-    if i==0:
+    if i == 0:
         print('0')
-    elif i==1:
+    elif i == 1:
         print('1')
     else:
-        fiboList[i] = fiboList[i-1] + fiboList[i-2]
+        fiboList[i] = fiboList[i - 1] + fiboList[i - 2]
 
 i = 1;
-while( i <= num ):
-    print(fiboList[i], end = " ")
+while (i <= num):
+    print(fiboList[i], end=" ")
     i += 1
 
-print( "\nF" + str(num)  + " = " + str( fiboList[i-1]) )
+print("\nF" + str(num) + " = " + str(fiboList[i - 1]))
