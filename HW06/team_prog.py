fa007eb15db55bd69be2b309cfa75c71d1008e96
jxbxhyxnx <jbh010204@naver.com>
2022-04-27 10:14:53 +0900

    create mypkg

--- HW06/team_prog.py
+++ HW06/team_prog.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-import HW06
+import mypkg
 while(1):
     num = input("Select menu: 1)b2h 2)integerUnionIntersection 3)fibo 4)exit ? ")
     if num == "exit": 
         quit()
     else:
         if num == '1':
-            #import HW06.b2h
+            import mypkg.b2h
             print("b2h")
 
         elif num == '2':
-            import HW06.integerUnionIntersection
+            import mypkg.integerUnionIntersection
 
         elif num == '3':
-            import HW06.fibo
+            import mypkg.fibo
         
         elif num == '4':
             print("exit the program...")
