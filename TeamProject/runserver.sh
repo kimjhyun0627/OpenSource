bc2e3cbed264b18e53bfbbb169be6cca31ded046
kimjhyun <kimjhyun0627@gmail.com>
2022-05-31 20:55:54 +0900

    오타수정

--- TeamProject/runserver.sh
+++ TeamProject/runserver.sh
@@ -6,7 +6,7 @@ echo "$PW" | sudo -kS apt-get update
 echo "$PW" | sudo -kS apt install software-properties-common
 echo "$PW" | sudo -kS apt install python3.9
 python3 --version
-echo "$PW" | sudo -kS apt install python3-pip build-essesioal
+echo "$PW" | sudo -kS apt install python3-pip build-essential
 
 echo "$PW" | sudo -kS apt-get update
 echo "$PW" | sudo -kS apt-get upgrade -y
