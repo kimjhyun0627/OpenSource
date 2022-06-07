773518f0d360c75ecbea24a7d4c67f429f18995d
kimjhyun <kimjhyun0627@gmail.com>
2022-06-07 18:47:50 +0900

    .

--- TeamProject/WHATSTAGRAM/app.py
+++ TeamProject/WHATSTAGRAM/app.py
@@ -8,14 +8,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 es_host = "http://localhost:9200"
 
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('main.html')
 
-@app.route('/crawl', methods=['GET', 'POST'])
-def dosomething():
-    # TODO: 내용 넣기
-    return
+
+# @app.route('/crawl', methods=['GET', 'POST'])
+# def dosomething():
+#     # TODO: 내용 넣기
+#     return
 
 if __name__ == "__main__":
     app.run(debug=True)
--- TeamProject/WHATSTAGRAM/runelasticsearch.sh
+++ TeamProject/WHATSTAGRAM/runelasticsearch.sh
@@ -2,8 +2,8 @@
 
 cd || exit
 
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.2.0-linux-x86_64.tar.gz
-tar xvzf elasticsearch-8.2.0-linux-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.2.0-linux-x86_64.tar.gz
+#tar xvzf elasticsearch-8.2.0-linux-x86_64.tar.gz
 
 # shellcheck disable=SC2164
 cd elasticsearch-8.2.0
