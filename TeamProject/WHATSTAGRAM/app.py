63e3acf501644cc46217903fa088875bd3d82a51
김진현 <79571893+kimjhyun0627@users.noreply.github.com>
2022-06-17 21:16:41 +0900

    Update app.py

--- TeamProject/WHATSTAGRAM/app.py
+++ TeamProject/WHATSTAGRAM/app.py
@@ -85,7 +85,7 @@ def instagram_crawling():
 
     # 인스타 사용자의 아이디 url사이트로 넘어가기
     br.get(url)
-    time.sleep(5)
+    time.sleep(10)
 
     # 해당 페이지의 div 클래스 id를 추출후 # 추가
     html = br.page_source
@@ -101,19 +101,23 @@ def instagram_crawling():
 
     # 크롤링할 게시물 행 by 열 범위 지정
 
+    time.sleep(2)
     try:
-        post = br.find_element_by_css_selector(
-                    id + f'> div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div._a3gq > section > main > div > div._aa-i > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > a > div._aagu > div._aagw')
+        #post = br.find_element_by_css_selector(
+         #           id + f'> div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div._a3gq > section > main > div > div._aa-i > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > a > div._aagu > div._aagw')
+        post = br.find_element(By.CSS_SELECTOR, id + f'> div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div._a3gq > section > main > div > div._aa-i > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > a > div._aagu > div._aagw')
         post.click()
-        time.sleep(2)
+        time.sleep(5)
         html = br.page_source
         soup = BeautifulSoup(html, 'lxml')
         print("success")
     except:
-        print("check")
+        print("fail")
     
     for row in range(1, 4):  # 3 by
         for col in range(1, 4):  # 3
+      	   
+            print(row,col)
             try:  #게시물이 비었는지 판단하기 위한 try 문 - 오류1해결
                
                 post = br.find_element_by_css_selector(
@@ -152,7 +156,7 @@ def instagram_crawling():
 
             except:
                 print("게시물이 비었습니다")
-                return
+                
 
 
 
