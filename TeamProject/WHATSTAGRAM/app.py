364103de227cd3c77074d1e6e8d1a6712fcd705a
kimjhyun <kimjhyun0627@gmail.com>
2022-06-18 18:39:26 +0900

    .

--- TeamProject/WHATSTAGRAM/app.py
+++ TeamProject/WHATSTAGRAM/app.py
@@ -51,6 +51,7 @@ def crawl():
 def set_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("headless")
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
@@ -171,7 +172,8 @@ def instagram_crawling(ID):
     print(tag_freq)
     print(human_list)
 
-    br.close()
+    #br.close()
+    br.quit()
 
     return tag_list[0:4], tag_freq[0:4]
 
